af768155ee6352166debb2ef5d5b5f944c6217e8
tmdgur328 <tmdgur328@naver.com>
2025-02-08 02:46:23 +0900

    ver 5

--- routesss/chat.py
+++ routesss/chat.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, request, redirect, Blueprint, make_response
+from flask import render_template, url_for, request, redirect, Blueprint, make_response, jsonify
 from flask_login import current_user
 from datetime import datetime
 from flask_socketio import SocketIO, join_room, leave_room, emit
@@ -17,20 +17,23 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @chatting.route('/chat_room', methods=['POST', 'GET'])
 @admin_only
 def chat_room():
-    # 캐시 제어를 위한 헤더 추가, 항상 최신 데이터를 표시하도록 보장함
-    # make_response(): 응답 객체를 생성하고 캐시 제어 헤더를 추가하는 함수
-    response = make_response(render_template('chat/new_chat_room.html', 
-                                          chat_room_list=get_chat_rooms(),
-                                          logged_in=current_user.is_authenticated))
-    # no-chche: 브라우저는 캐시된 데이터를 사용하기 전에 항상 서버에서 최신 데이터를 요청함
-    # no-store: 브라우저는 캐시된 데이터를 저장하지 않음
-    # must-revalidate: 캐시된 데이터가 유효하지 않은 경우, 반드시 서버에 데이터를 확인해야 함
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    # Pragma: no-cache 캐시를 비활성화 하기 위해 사용
-    response.headers['Pragma'] = 'no-cache'
-    # Expires: 0 캐시 만료 시간을 0으로 설정하여 캐시된 데이터가 만료되도록 함
-    response.headers['Expires'] = '0'
-    return response
+    # # 캐시 제어를 위한 헤더 추가, 항상 최신 데이터를 표시하도록 보장함
+    # # make_response(): 응답 객체를 생성하고 캐시 제어 헤더를 추가하는 함수
+    # response = make_response(render_template('chat/new_chat_room.html', 
+    #                                       chat_room_list=get_chat_rooms(),
+    #                                       logged_in=current_user.is_authenticated))
+    # # no-chche: 브라우저는 캐시된 데이터를 사용하기 전에 항상 서버에서 최신 데이터를 요청함
+    # # no-store: 브라우저는 캐시된 데이터를 저장하지 않음
+    # # must-revalidate: 캐시된 데이터가 유효하지 않은 경우, 반드시 서버에 데이터를 확인해야 함
+    # response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    # # Pragma: no-cache 캐시를 비활성화 하기 위해 사용
+    # response.headers['Pragma'] = 'no-cache'
+    # # Expires: 0 캐시 만료 시간을 0으로 설정하여 캐시된 데이터가 만료되도록 함
+    # response.headers['Expires'] = '0'
+    # return response
+    return render_template('chat/new_chat_room.html', 
+                          chat_room_list=get_chat_rooms(),
+                          logged_in=current_user.is_authenticated)
 
 # 채팅방 목록 가져오기
 def get_chat_rooms():
@@ -74,12 +77,6 @@ def get_chat_rooms():
             'latest_message': latest_message.text,
             'message_time': message_time,
             'other_user_profile': other_user.profile_image_name if other_user else None,
-
-            'user_id': current_user.name,
-            # 'receive_user_name': receive_user_name,
-            'messages': messages,
-            'logged_in': current_user.is_authenticated,
-            # 'receive_user_id': receive_user_id
         }
 
     for room in rooms:
@@ -87,62 +84,74 @@ def get_chat_rooms():
         
     return chat_room_list
 
+# 채팅 페이지
+@chatting.route('/get_messages/<receive_user_id>', methods=['GET'])
+@admin_only
+def chat(receive_user_id):
+    # receive_user_id: 메시지를 받게 되는 사람
+    receive_user_id = receive_user_id
+    # print(f"Received user_id: {receive_user_id}")  # 디버깅을 위한 출력
+    receive_user_name = request.args.get('receive_user_name')
+    # print(f"Received user_name: {receive_user_name}")
+    room_id = request.args.get('room_id')
+    # print(f"Received room_id: {room_id}")
 
-# # receive_user_id: 메시지를 받게 되는 사람
-#     # print(f"Received user_id: {receive_user_id}")  # 디버깅을 위한 출력
-#     receive_user_name = request.args.get('receive_user_name')
-#     # print(f"Received user_name: {receive_user_name}")
-#     room_id = request.args.get('room_id')
-#     # print(f"Received room_id: {room_id}")
+    # current_user: 채팅하기 눌렀을 때 메시지를 처음 보내는 사람
+    room = Room.query.filter(
+        or_(
+            and_(Room.sender_id == current_user.id, Room.receiver_id == receive_user_id),
+            and_(Room.sender_id == receive_user_id, Room.receiver_id == current_user.id)
+        )
+    ).first()
 
-#     # current_user: 채팅하기 눌렀을 때 메시지를 처음 보내는 사람
-#     room = Room.query.filter(
-#         or_(
-#             and_(Room.sender_id == current_user.id, Room.receiver_id == receive_user_id),
-#             and_(Room.sender_id == receive_user_id, Room.receiver_id == current_user.id)
-#         )
-#     ).first()
+    if not room:
+        new_chat_room = Room(
+            sender_id=current_user.id,
+            receiver_id=receive_user_id,
+            sender_join=True,
+            receiver_join=True
+        )
+        db.session.add(new_chat_room)
+        db.session.commit()
 
-#     if not room:
-#         new_chat_room = Room(
-#             sender_id=current_user.id,
-#             receiver_id=receive_user_id,
-#             sender_join=True,
-#             receiver_join=True
-#         )
-#         db.session.add(new_chat_room)
-#         db.session.commit()
+        room = new_chat_room
 
-#         room = new_chat_room
+    # room_id가 존재하면 해당 room_id로, 그렇지 않으면 room.id로 chat_room을 가져옴
+    chat_room = db.get_or_404(Room, room_id if room_id else room.id)
+    # print(chat_room)
 
-#     # room_id가 존재하면 해당 room_id로, 그렇지 않으면 room.id로 chat_room을 가져옴
-#     chat_room = db.get_or_404(Room, room_id if room_id else room.id)
-#     # print(chat_room)
+    if current_user.id != chat_room.sender_id and current_user.id != chat_room.receiver_id:
+        return redirect(url_for('index'))
 
-#     if current_user.id != chat_room.sender_id and current_user.id != chat_room.receiver_id:
-#         return redirect(url_for('index'))
+    if current_user.id == chat_room.sender_id:
+        if chat_room.sender_join == False:
+            chat_room.sender_join = True
+            chat_room.sender_last_join = datetime.now()
+            db.session.commit()
 
-#     if current_user.id == chat_room.sender_id:
-#         if chat_room.sender_join == False:
-#             chat_room.sender_join = True
-#             chat_room.sender_last_join = datetime.now()
-#             db.session.commit()
+        if chat_room.sender_last_join is None:
+            messages = Message.query.filter_by(room_id=chat_room.id).order_by(Message.time).all()  # 시간순 정렬 추가
+        else:
+            messages = Message.query.filter_by(room_id=chat_room.id).filter(Message.time >= chat_room.sender_last_join).order_by(Message.time).all()
+        
+        return render_template('chat/chatting.html', user=current_user.name, room_id=chat_room.id,
+                               receive_user_name=receive_user_name, messages=messages, logged_in=current_user.is_authenticated,
+                               receive_user_id=receive_user_id)
 
-#         if chat_room.sender_last_join is None:
-#             messages = Message.query.filter_by(room_id=chat_room.id).order_by(Message.time).all()  # 시간순 정렬 추가
-#         else:
-#             messages = Message.query.filter_by(room_id=chat_room.id).filter(Message.time >= chat_room.sender_last_join).order_by(Message.time).all()
+    elif current_user.id == chat_room.receiver_id:
+        if chat_room.receiver_join == False:
+            chat_room.receiver_join = True
+            chat_room.receiver_last_join = datetime.now()
+            db.session.commit()
 
-#     elif current_user.id == chat_room.receiver_id:
-#         if chat_room.receiver_join == False:
-#             chat_room.receiver_join = True
-#             chat_room.receiver_last_join = datetime.now()
-#             db.session.commit()
+        if chat_room.receiver_last_join is None:
+            messages = Message.query.filter_by(room_id=chat_room.id).all()  # 모든 메시지
+        else:
+            messages = Message.query.filter_by(room_id=chat_room.id).filter(Message.time >= chat_room.receiver_last_join).all()
 
-#         if chat_room.receiver_last_join is None:
-#             messages = Message.query.filter_by(room_id=chat_room.id).all()  # 모든 메시지
-#         else:
-#             messages = Message.query.filter_by(room_id=chat_room.id).filter(Message.time >= chat_room.receiver_last_join).all()
+        return render_template('chat/chatting.html', user=current_user.name, room_id=chat_room.id,
+                               receive_user_name=receive_user_name, messages=messages, logged_in=current_user.is_authenticated,
+                               receive_user_id=receive_user_id)
 
 @socketio.on("connect")
 def test_connect():
